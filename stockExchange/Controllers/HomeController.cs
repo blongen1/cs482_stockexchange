d2ceef9bece2dd360c5d15def78bba833c356ecd
Brandon Longenecker <blongen1@ycp.edu>
2018-02-04 12:35:25 -0500

    Initialize Repository

---
+++ stockExchange/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace stockExchange.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Stock Simulator is created for CS482 - Senior Software Project II";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Feel free to send me a message!";
+
+            return View();
+        }
+    }
+}
---
+++ stockExchange/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="stockExchange.MvcApplication" Language="C#" %>
