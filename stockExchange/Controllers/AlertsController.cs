79467a325a6e2842d76cacfd5b912a503359587d
Brandon Longenecker <blongen1@ycp.edu>
2018-05-02 21:30:30 -0400

    Can now view and create custom percentage email alerts

--- stockExchange/Controllers/AlertsController.cs
+++ stockExchange/Controllers/AlertsController.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
 using stockExchange.Models;
+using stockExchange.ViewModels;
 
 namespace stockExchange.Controllers
 {
@@ -25,12 +29,71 @@ namespace stockExchange.Controllers
         // GET: Alerts
         public ActionResult Index()
         {
-            if (!Request.IsAuthenticated)
+            var userId = User.Identity.GetUserName();
+
+            if (userId == null)
             {
                 return RedirectToRoute(new { controller = "Account", action = "Login" });
             }
 
-            return View();
+            var alerts = _context.Alerts.ToList().Where(t => t.EmailAddress == userId);
+
+            var viewModel = new AlertsViewModel
+            {
+                Alertses = alerts
+            };
+            
+
+            return View(viewModel);
         }
+
+        [HttpPost]
+        public ActionResult AddAlert(AlertsViewModel alertModel)
+        {
+
+            if (alertModel.Alerts.PercentChange == 0)
+            {
+                TempData["error"] = "Your entry is invalid. Try entering a positive or negative integer";
+                return RedirectToAction("Index", "Alerts");
+            }
+            
+            var currentAlerts = _context.Alerts.Where(t => t.EmailAddress == alertModel.Alerts.EmailAddress);
+
+            if (currentAlerts.ToList().Count >= 2)
+            {
+                TempData["error"] = "You can not set up more than two alerts";
+                return RedirectToAction("Index", "Alerts");
+            }
+
+            if (currentAlerts.SingleOrDefault(t => t.PercentChange == alertModel.Alerts.PercentChange) == null)
+            {
+
+                _context.Alerts.Add(alertModel.Alerts);
+                _context.SaveChanges();
+
+            }
+            else
+            {
+                TempData["error"] = "You can not enter the same value as a previously set up alert";
+                return RedirectToAction("Index", "Alerts");
+            }
+
+
+            return RedirectToAction("Index", "Alerts");
+        }
+
+        [HttpPost]
+        public ActionResult DeleteAlert(AlertsViewModel alertModel)
+        {
+
+            var alert = new Alerts() {Id = alertModel.Alerts.Id};
+            _context.Entry(alert).State = EntityState.Deleted;
+            _context.SaveChanges();
+
+
+            return RedirectToAction("Index", "Alerts");
+        }
+
+
     }
 }
---
+++ stockExchange/ViewModels/AlertsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using stockExchange.Models;
+
+namespace stockExchange.ViewModels
+{
+    public class AlertsViewModel
+    {
+        public Alerts Alerts { get; set; }
+        public IEnumerable<Alerts> Alertses { get; set; }
+    }
+}
